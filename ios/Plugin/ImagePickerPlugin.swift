58152bb2be08d481e4d0d1b70c9777928afc818e
jonz94 <jody16888@gmail.com>
2022-10-11 11:26:38 +0800

    feat(ios): add implementation

--- ios/Plugin/ImagePickerPlugin.swift
+++ ios/Plugin/ImagePickerPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import YPImagePicker
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -7,12 +8,53 @@ import Capacitor
  */
 @objc(ImagePickerPlugin)
 public class ImagePickerPlugin: CAPPlugin {
-    private let implementation = ImagePicker()
+    private var imageCounter = 0
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    @objc func present(_ call: CAPPluginCall) {
+        var config = YPImagePickerConfiguration()
+
+        config.showsPhotoFilters = false
+        config.onlySquareImagesFromCamera = false
+        config.screens = [.library]
+        config.library.minNumberOfItems = 1
+        config.library.maxNumberOfItems = 1
+
+        DispatchQueue.main.async {
+            let picker = YPImagePicker(configuration: config)
+
+            picker.didFinishPicking { items, cancelled in
+                if cancelled {
+                    CAPLog.print("User Cancelled")
+                } else if let photo = items.singlePhoto?.image.jpegData(compressionQuality: 1) {
+                    guard let url = try? self.saveTemporaryImage(photo) else {
+                        call.reject("Unable to get portable path to file")
+                        return
+                    }
+
+                    call.resolve([
+                        "value": url.absoluteString
+                    ])
+                }
+
+                picker.dismiss(animated: true, completion: nil)
+            }
+
+            self.bridge?.viewController?.present(picker, animated: true, completion: nil)
+        }
+    }
+
+    /**
+     * Credits: https://github.com/ionic-team/capacitor-plugins/blob/b3a42bb198313066ea502ee94882473247fe0c03/camera/ios/Plugin/CameraPlugin.swift#L518-L527
+     */
+    private func saveTemporaryImage(_ data: Data) throws -> URL {
+        var url: URL
+        repeat {
+            imageCounter += 1
+            url = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("image-picker-\(imageCounter).jpg")
+        } while FileManager.default.fileExists(atPath: url.path)
+
+        try data.write(to: url, options: .atomic)
+
+        return url
     }
 }
